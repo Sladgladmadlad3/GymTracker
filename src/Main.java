0901f16efac895ece2849a51e328355bde6686e6
Michael Franklin <mf021036@gmail.com>
2024-11-26 09:01:53 -0700

    feat: established a connection to MySQL Databasae

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Driver;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GymTracker!");
+        String url ="jdbc:mysql://localhost:3306/gym_tracker";
+        String user = "root";
+        String password = "Basketball3!";
+
+        //Class.forName("com.mysql.cj.jdbc.Driver");
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the database!");
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to Connect");
+            e.printStackTrace();
+        }
     }
 }
 
